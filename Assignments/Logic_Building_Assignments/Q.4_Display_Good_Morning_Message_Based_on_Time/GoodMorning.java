ab3ae68162634e9aedf7ad4dec78fb6474ee0558
VinitDharmikAC <vinit.dharmik.cmaug26@gmail.com>
2026-08-18 22:31:45 +0530

    Add files via upload

---
+++ Assignments/Logic_Building_Assignments/Q.4_Display_Good_Morning_Message_Based_on_Time/GoodMorning.java
@@ -0,0 +1,9 @@
+public class GoodMorning {
+    public static void main(String[] args) {
+        int num1 = 1;
+
+        if (num1 >=5 && num1 <=12 ) {
+            System.out.println("Good Morning!");
+        }
+    }
+}
