4fe54cc0fed7e94ff0f21d7397bf7d7d0d2c9e50
VinitDharmikAC <vinit.dharmik.cmaug26@gmail.com>
2026-08-18 22:34:45 +0530

    Add files via upload

---
+++ Assignments/Logic_Building_Assignments/Q.6_Print_Area_of_a_Rectangle/AreaofRectangle.java
@@ -0,0 +1,10 @@
+public class AreaofRectangle {
+    public static void main(String[] args) {
+        int length = 10;
+        int width = 20;
+
+        int Area = length * width;
+
+        System.out.println("Area Of Rectangle is = " + Area);
+    }
+}
