9ed6bb9395598ab90e4c0bf2aabfe3846ef4a753
VinitDharmikAC <vinit.dharmik.cmaug26@gmail.com>
2026-08-18 22:33:30 +0530

    Add files via upload

---
+++ Assignments/Logic_Building_Assignments/Q.5_Print_Area_of_a_Square/AreaofSquare.java
@@ -0,0 +1,9 @@
+public class AreaofSquare {
+    public static void main(String[] args) {
+        int side = 10;
+
+        int Area = side * side;
+
+        System.out.println("Area Of Square is = " + Area);
+    }
+}
