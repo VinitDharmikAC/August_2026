4f0ddf04d04fbc7c65fdea184312e5e96b1de109
VinitDharmikAC <vinit.dharmik.cmaug26@gmail.com>
2026-08-19 08:33:50 +0530

    Add files via upload

---
+++ Assignments/Logic Building Assignment Day 2/Q3 Days of the Week/DayOfWeek.java
@@ -0,0 +1,38 @@
+public class DayOfWeek{
+public static void main(String[] args){
+ int day = 5;
+ 
+switch(day){
+      case 1:
+      System.out.println("Its a Monday.");
+      break;
+
+      case 2:
+      System.out.println("Its a Tuesday.");
+      break;
+
+      case 3:
+      System.out.println("Its a Wednesday.");
+      break;
+
+      case 4:
+      System.out.println("Its a Thursday.");
+      break;
+
+      case 5:
+      System.out.println("Its a Friday.");
+      break;
+
+      case 6:
+      System.out.println("Its a Saturday.");
+      break;
+
+      case 7:
+      System.out.println("Its a Sunday.");
+      break;
+     
+     default:
+      System.out.println("Invalid.");
+}
+      }
+}
