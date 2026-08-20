dec01c826ea683d2d71d608f1174cf816b27f2ee
VinitDharmikAC <vinit.dharmik.cmaug26@gmail.com>
2026-08-20 09:27:48 +0530

    Add files via upload

---
+++ Assignments/Logic Building Assignment Day 2/Q4 Identify the Values of Uninitialized Variables/Datatype.java
@@ -0,0 +1,36 @@
+public class DataType{
+   public static void main(String[] args){
+   
+   byte a = 12;
+   System.out.println(a);
+   
+   short b = 25;
+   System.out.println(b);
+   
+   int c = 2634;
+   System.out.println(c);
+   
+   long d = 25688;
+   System.out.println(d);
+   
+   float e = 26.356f;
+   System.out.println(e);
+   
+   double f = 3568455;
+   System.out.println(f);
+   
+   char g = 'C';
+   System.out.println(g);
+   
+   boolean h = true;
+   System.out.println(h);
+   
+   
+   
+   
+   
+   }
+
+
+
+}
