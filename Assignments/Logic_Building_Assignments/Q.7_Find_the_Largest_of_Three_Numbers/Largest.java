c65af0552ca90f63bacb87c6f610ac36c92847ae
VinitDharmikAC <vinit.dharmik.cmaug26@gmail.com>
2026-08-20 09:12:52 +0530

    Add files via upload

---
+++ Assignments/Logic_Building_Assignments/Q.7_Find_the_Largest_of_Three_Numbers/Largest.java
@@ -0,0 +1,24 @@
+public class Largest{
+
+    public static void main(String[] args){
+	     int A = 12;
+		 int B = 13;
+		 int C = 56;
+		
+	if( A > B && A > C ){
+	System.out.println("A is the largest");
+	}
+	if(B > A && B > C){
+	System.out.println("B is the largest");
+	}
+	if(C > A && C > B){
+	System.out.println("C is the largest");
+	}
+	
+	
+	
+	
+	}
+
+
+}
