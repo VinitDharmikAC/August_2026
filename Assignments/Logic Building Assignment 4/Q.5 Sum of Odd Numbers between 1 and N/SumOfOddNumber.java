bfaabaf6a2ec5dc8d1aa8b3d152ce6c8683c6fe1
VinitDharmikAC <vinit.dharmik.cmaug26@gmail.com>
2026-08-23 00:21:49 +0530

    Add files via upload

---
+++ Assignments/Logic Building Assignment 4/Q.5 Sum of Odd Numbers between 1 and N/SumOfOddNumber.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+
+public class SumOfOddNumber{
+
+    public static void main(String[] args){
+
+
+           Scanner sc = new Scanner(System.in);
+             System.out.println("Enter a number = ");
+        int n = sc.nextInt();
+
+         int sum = 0;
+
+           for (int i = 1; i <=n ; i++){
+              if(i % 2!= 0){
+
+            sum = sum + i;
+             }
+
+        }
+
+        System.out.println("Sum of the odd number is " + sum);
+
+
+
+    }
+
+}
