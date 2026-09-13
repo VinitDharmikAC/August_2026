bb60a1b3f6d414a5fc089570e0aef304589f0718
VinitDharmikAC <vinit.dharmik.cmaug26@gmail.com>
2026-09-14 00:51:55 +0530

    Add files via upload

---
+++ Oops with Java Assignment/Forloop_Assignment/src/PatternFive.java
@@ -0,0 +1,23 @@
+
+public class PatternFive {
+
+	public static void main(String[] args) {
+
+
+		for(int i = 0;i<=5;i++) {
+			
+			for(int j = 1;j<=6-i;j++) {
+				
+				System.out.print(j + " ");
+			}
+			
+			
+			
+			
+			System.out.println();
+			
+		}
+
+	}
+
+}
